868938d957333b1ac22fdff58dd718d3c8363ae0
Syric <76715986+Syrikal@users.noreply.github.com>
2023-01-14 17:12:56 -0500

    Removed cursor if not in dimension

--- src/main/java/com/syric/betternethermap/mixin/MixinMapData.java
+++ src/main/java/com/syric/betternethermap/mixin/MixinMapData.java
@@ -1,15 +1,28 @@
 package com.syric.betternethermap.mixin;
 
 import com.syric.betternethermap.BNMConfig;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.MapData;
+import net.minecraft.world.storage.MapDecoration;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
 
 @Mixin(MapData.class)
 public class MixinMapData {
+
+    @Shadow @Final public Map<String, MapDecoration> decorations;
+
+    @Shadow public RegistryKey<World> dimension;
 
     @Redirect(method = "addDecoration", at = @At(value = "FIELD", target = "Lnet/minecraft/world/storage/MapData;dimension:Lnet/minecraft/util/RegistryKey;"))
     public RegistryKey<World> addDecoration(MapData instance) {
@@ -21,6 +34,11 @@ public class MixinMapData {
         }
     }
 
-    //Disable cursor if player isn't in the dimension!
+    @Inject(method= "tickCarriedBy", at = @At(value = "TAIL"))
+    public void removeOtherDimensions(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
+        if (player.level.dimension() != this.dimension) {
+            this.decorations.remove(player.getName().getString());
+        }
+    }
 
 }
