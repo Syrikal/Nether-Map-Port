1292ada3132a955c9190c3f079266b8e9921d147
Syric <76715986+Syrikal@users.noreply.github.com>
2023-02-22 22:10:41 -0500

    Finished bugfix in 1.18

--- src/main/java/com/syric/betternethermap/mixin/MixinEmptyMapItem.java
+++ src/main/java/com/syric/betternethermap/mixin/MixinEmptyMapItem.java
@@ -3,7 +3,6 @@ package com.syric.betternethermap.mixin;
 import com.syric.betternethermap.config.BNMConfig;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.EmptyMapItem;
 import net.minecraft.world.item.ItemStack;
@@ -17,13 +16,13 @@ import static com.syric.betternethermap.getMapHeight.getVanillaMapHeight;
 @Mixin(EmptyMapItem.class)
 public class MixinEmptyMapItem {
 
-    @ModifyVariable(method = "use", at = @At("STORE"), ordinal = 0)
-    private ItemStack filledMap(ItemStack filledMapStack, Level world, Player player, InteractionHand hand) {
+    @ModifyVariable(method = "use", at = @At("STORE"), ordinal = 1)
+    private ItemStack filledMap(ItemStack filledMapStack, Level world, Player player) {
 
         //Add the NBT
         CompoundTag tag = filledMapStack.getOrCreateTag();
         tag.putInt("yLevel", getVanillaMapHeight(player, world));
-        if (world.isClientSide && BNMConfig.debugMessages.get()) {
+        if (BNMConfig.debugMessages.get()) {
             player.displayClientMessage(Component.nullToEmpty("Set map y-level to " + getVanillaMapHeight(player, world)), false);
         }
         filledMapStack.setTag(tag);
